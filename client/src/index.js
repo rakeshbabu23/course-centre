b279c8d966a8c6cb6b339bd7f13e41e8e8e3ba86
rakeshbabu23 <120632351+rakeshbabu23@users.noreply.github.com>
2023-07-25 14:43:45 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+
 import { BrowserRouter } from "react-router-dom";
 import { Provider, useSelector } from "react-redux";
 import store from "./course-app/store";
-import App2 from "./App2";
+
 
 import CourseApp from "./course-app/CourseApp";
 
